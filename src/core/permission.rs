1d2cbf4ad21e1628b72391fbcb3c02d7c1320da4
Juliandev02 <julian@strawberryfoundations.org>
2026-05-25 13:10:59 +0200

    fix: add #[must_use] attribute to user_in_input_group function

--- src/core/permission.rs
+++ src/core/permission.rs
@@ -4,6 +4,7 @@ use std::path::PathBuf;
 use std::process::Command;
 use crate::core::device::has_device_access;
 
+#[must_use] 
 pub fn user_in_input_group() -> bool {
     if let Ok(output) = Command::new("id").arg("-nG").output()
         && let Ok(s) = String::from_utf8(output.stdout) {
